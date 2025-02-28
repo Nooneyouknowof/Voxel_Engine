b82873a69dff2814af9f8f43fd72caac02c5ba76
Nooneyouknowof <octaviomcnaughton@gmail.com>
2025-02-27 22:11:42 -0700

    revamped code to work with winit 0.30+ versions for better support

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 mod window;
 mod vulkan;
 
-use winit::event_loop::{ControlFlow, EventLoop};
+use winit::event_loop::EventLoop;
 use window::*;
-use vulkan::surface::VulkanApp;
-use vulkan::device::VulkanPhysicalDevice;
+// use vulkan::surface::VulkanApp;
+// use vulkan::device::VulkanPhysicalDevice;
 
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Poll);
 
-    let mut app = App::default();
-    event_loop.run_app(&mut app).expect("Run App failed!");
-    let vulkan_app = VulkanApp::new(app.window.as_ref().unwrap());
-    
-    let instance = vulkan_app.instance.clone();
-    let physical_device = VulkanPhysicalDevice::pick_physical_device(&instance);
-    println!("Selected Physical Device: {:?}", physical_device.physical_device);
-    
+    let mut events = AppEvents::default();
+    event_loop.run_app(&mut events).expect("Run App failed!");
 }
--- src/window.rs
+++ src/window.rs
@@ -1,41 +1,108 @@
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
-use winit::event_loop::ActiveEventLoop;
-use winit::window::{Window, WindowId};
+use winit::window;
+use winit::{event_loop::ActiveEventLoop, window::{Window, WindowId}};
+
+use ash::{vk, Entry, Instance};
+use ash_window;
+use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
+use std::{ffi::CString, os::raw::c_char};
 
 #[derive(Default)]
-pub struct App {
-    pub window: Option<Window>,
+pub struct AppEvents {
+    window: Option<Window>,
+    instance: Option<Instance>,  // Store Vulkan instance
+    surface: Option<vk::SurfaceKHR>,  // Store Vulkan surface
+    entry: Option<Entry>,  // Store Vulkan entry
 }
 
-impl ApplicationHandler for App {
+impl ApplicationHandler for AppEvents {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+        let attributes = Window::default_attributes()
+            .with_title("Sage Zinnia (Beta)");
+        self.window = Some(event_loop.create_window(attributes.clone()).unwrap());
+        let window = self.window.as_ref().unwrap();
+
+        let entry = unsafe { Entry::load().expect("Failed to load Vulkan") };
+        // Application info
+        let app_name = CString::new(attributes.title.to_string()).unwrap();
+        let engine_name = CString::new("Custom Engine").unwrap();
+        let app_info = vk::ApplicationInfo {
+            s_type: vk::StructureType::APPLICATION_INFO,
+            p_application_name: app_name.as_ptr(),
+            application_version: vk::make_api_version(0, 1, 0, 0),
+            p_engine_name: engine_name.as_ptr(),
+            engine_version: vk::make_api_version(0, 1, 0, 0),
+            api_version: vk::make_api_version(0, 1, 3, 0),
+            ..Default::default()
+        };
+
+        // Get required extensions from winit
+        let extension_names = required_extensions(window);
+
+        let instance_info = vk::InstanceCreateInfo {
+            s_type: vk::StructureType::INSTANCE_CREATE_INFO,
+            p_application_info: &app_info,
+            enabled_extension_count: extension_names.len() as u32,
+            pp_enabled_extension_names: extension_names.as_ptr(),
+            ..Default::default()
+        };
+
+        let instance = unsafe {
+            entry
+                .create_instance(&instance_info, None)
+                .expect("Failed to create Vulkan instance")
+        };
+
+        // Create Vulkan surface
+        let surface = unsafe {
+            ash_window::create_surface(
+                &entry,
+                &instance,
+                window.display_handle().unwrap().into(),
+                window.window_handle().unwrap().into(),
+                None
+            ).expect("Failed to create Vulkan surface")
+        };
+
+        self.entry = Some(entry);
+        self.instance = Some(instance);
+        self.surface = Some(surface);
+
+        println!("Vulkan surface successfully created!");
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
+                println!("The close button was pressed");
                 event_loop.exit();
-            },
-            WindowEvent::RedrawRequested => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-
-                // Draw.
-
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw in
-                // applications which do not always need to. Applications that redraw continuously
-                // can render here instead.
-                self.window.as_ref().unwrap().request_redraw();
             }
-            _ => (),
+
+            WindowEvent::RedrawRequested => {
+                // TODO: Draw frame using Vulkan
+            }
+            _ => ()
         }
     }
+
+    fn exiting(&mut self, _: &ActiveEventLoop) {
+        println!("Exiting window");
+        // Destroy Vulkan resources safely
+        // if let Some(instance) = &self.instance {
+        //     if let Some(surface) = self.surface.take() {
+        //         unsafe {
+        //             let surface_loader = ash::khr::Surface::new(self.entry.as_ref().unwrap(), instance);
+        //             surface_loader.destroy_surface(surface, None);
+        //         }
+        //     }
+        // }
+    }
+}
+
+fn required_extensions(window: &Window) -> Vec<*const c_char> {
+    let surface_extensions = ash_window::enumerate_required_extensions(window.display_handle().unwrap().into()).unwrap();
+    let mut extensions = Vec::with_capacity(surface_extensions.len());
+    extensions.extend(surface_extensions.iter().copied());
+    extensions
 }
